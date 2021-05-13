0df273908eb7527d81a6f58f4951389412ca26dd
Chris Crocker-White <chriscw@balena.io>
2021-05-13 21:29:53 +0100

    Update for open fleet
    Change-type: patch
    Signed-off-by: Chris Crocker-White <chriscw@balena.io>

---
+++ octoprint/start.sh
@@ -0,0 +1,5 @@
+curl -X PATCH --header "Content-Type:application/json" \
+    --data '{"network": {"hostname": "balena"}}' \
+    "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY"
+
+exec octoprint serve --iknowwhatimdoing --host 0.0.0.0
